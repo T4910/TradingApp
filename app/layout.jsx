cae39619df0ecec4677708956e2f465942d76b19
Emmanuel Taiwo <ttaiwo4910@gmail.com>
2023-06-06 16:03:34 +0100

    Nextjs-boilerplate

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+    return (
+      <html lang="en">
+        <head />
+        <body style={{margin: '0px'}}>
+            {children}
+        </body>
+      </html>
+    );
+  }
