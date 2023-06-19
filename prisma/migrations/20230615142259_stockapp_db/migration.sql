cdc9c62451ee6842a1c9104bdc35147f71f888e7
Emmanuel Taiwo <ttaiwo4910@gmail.com>
2023-06-19 10:39:10 +0100

    added prisma for database ORM

---
+++ prisma/migrations/20230615142259_stockapp_db/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `user` ADD COLUMN `account_balance` DOUBLE NOT NULL DEFAULT 10000;
---
+++ prisma/migrations/20230618153853_added_email/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `user` ADD COLUMN `email` VARCHAR(255) NOT NULL DEFAULT 'taiwo.emmanuel@lmu.edu.ng';
