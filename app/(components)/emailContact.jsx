205b6abd30faa570ff4c87607f386f5bfe01d8ed
Emmanuel Taiwo <ttaiwo4910@gmail.com>
2023-06-24 13:13:50 +0100

    added about and contact page

--- app/(components)/emailContact.jsx
+++ app/(components)/emailContact.jsx
@@ -1,22 +1,47 @@
+'use client'
+import {useRef, useState} from 'react'
+import InfoField from './infoField' 
+
 export default function emailContact() {
     let nameBox = useRef()
     let emailBox = useRef()
     let messageBox = useRef()
+    const [{description, visibility, content}, setinfoFieldContent] = useState({
+      description: 'info',
+      visibility: false,
+      content: '...'
+  })
 
     function emailMessage(e){
         e.preventDefault()
-
-        // 
-
         // message email
+
+        const letter = {
+          name: nameBox.current.value,
+          email: emailBox.current.value,
+          message: messageBox.current.value
+        }
+
+        if(letter.name === '' || letter.email === '' || letter.message === '') return setinfoFieldContent({
+          description: 'error',
+          visibility: true,
+          content: 'Missing fields'
+        })
+
+        console.log(letter)
     }
 
   return (
     <div>
+        <InfoField description={description} visibility={visibility} settingFunction={setinfoFieldContent}>{content}</InfoField>
         <form onSubmit={(e) => emailMessage(e)}>
+          <label htmlFor="name">Fullname: </label>
             <input type="text" name="name" ref={nameBox} required={true}/>
+          <label htmlFor="email">Email:</label>
             <input type="email" name="email" ref={emailBox} required={true}/>
-            <input type="container" name="message" ref={messageBox} required={true}/>
+          <label htmlFor="message">Messege</label>
+            <textarea ref={messageBox} name="message" id="message" cols="30" rows="10" required={true} placeholder={'Type your message here...'}></textarea>
+          <input type="submit" value="Send" />
         </form>
     </div>
   )
--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,20 +1,25 @@
+import NavBar from '../(components)/NavBar'
+import Footer from '../(components)/Footer'
 import EmailContact from '../(components)/emailContact'
-import useAlertPopup from '../(components)/hooks/alertPopup'
+
 
 export default function page() {
-    const popup = useAlertPopup()
   return (
-    <div>
-        <p>Hey, I'm Emmanuel. The cretor of this website</p>
-        <p>
-            I am currently a student with a passion to help people in any way by creating application and outreach
-            to people who are in need from the money I make.
-        </p>
-        <p>If you ever want to contact me you can use the following links below</p>
-        {/* links in icons */}
-        <p>Or send an email</p>
+    <>
+      <NavBar />
+      <div>
+          <p>Hey, I'm Emmanuel. The cretor of this website</p>
+          <p>
+              I am currently a student with a passion to help people in any way by creating application and outreach
+              to people who are in need from the money I make.
+          </p>
+          <p>If you ever want to contact me you can use the following links below</p>
+          {/* links in icons */}
+          <p>Or send an email</p>
 
-        <EmailContact />
-    </div>
+          <EmailContact />
+          <Footer />
+      </div>
+    </>
   )
 }
