efee5a5962cdba3ca4769656eb7f988845f7e20d
Emmanuel Taiwo <ttaiwo4910@gmail.com>
2023-09-12 16:26:04 +0100

    fixed navbar dropdown issue

--- app/(components)/phoneNavBarDropdown.jsx
+++ app/(components)/phoneNavBarDropdown.jsx
@@ -1,16 +1,20 @@
+'use client'
 import LogOut from './logOutBtn'
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
 
 
 const DropDown = ({visibility, auth}) => {
+    let [authed, setAuthed] = useState(auth)
+    useEffect(() => setAuthed(auth), [auth])
     const isHidden = visibility ? '' : 'hidden'
     return(
         <div className={`flex flex-col mt-4 ${isHidden}`}>
-            {!auth?.user && <Link href="/" className='mb-1'>Home</Link>}
-            {!auth?.user && <Link href="/signings/login" className='mb-1'>Sign in</Link>}
-            {!auth?.user && <Link href="/signings/register" className='mb-1'>Sign up</Link>}
+            {!authed?.user && <Link href="/" className='mb-1'>Home</Link>}
+            {!authed?.user && <Link href="/signings/login" className='mb-1'>Sign in</Link>}
+            {!authed?.user && <Link href="/signings/register" className='mb-1'>Sign up</Link>}
             <Link href="/about" className='mb-1'>About Dev</Link>
-            {auth?.user && <LogOut />}
+            {authed?.user && <LogOut />}
         </div>
     )
 }
