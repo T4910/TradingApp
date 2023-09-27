dc7c4ed545875e4c882f6efc19971c611e483b88
Emmanuel Taiwo <ttaiwo4910@gmail.com>
2023-09-28 00:34:09 +0100

    removing env: NEXT_PUBLIC_WEB_URL usage in app

--- middleware.js
+++ middleware.js
@@ -3,7 +3,8 @@ import { NextResponse } from 'next/server'
 export const config = {matcher: '/dashboard/:path*'}
 
 export async function middleware(req) {   
-    const res = await fetch(`${process.env.NEXT_PUBLIC_WEB_URL}/api/auth/session`, { headers: req.headers, cache: 'no-cache' })
+    console.log(req.nextUrl)
+    const res = await fetch(`${req.nextUrl.origin}/api/auth/session`, { headers: req.headers, cache: 'no-cache' })
     const {user: session} = await res.json()
     console.log('session in middleware: ',req.url, session)
     // const cookieSession = req.cookies.get("next-auth.session-token")
