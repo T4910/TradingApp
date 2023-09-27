de776e16f1f09f8d5793411ee992405f07d75454
Emmanuel Taiwo <ttaiwo4910@gmail.com>
2023-09-27 01:15:15 +0100

    fixing middleware

--- middleware.js
+++ middleware.js
@@ -1,14 +1,17 @@
 import { RequestCookies } from 'next/dist/compiled/@edge-runtime/cookies'
 import { NextResponse } from 'next/server'
+import { getServerSession } from 'next-auth'
+import { authOptions } from './pages/api/auth/[...nextauth]'
 
-export function middleware(req) {   
+export async function middleware(req, res) {   
     const cookies = new RequestCookies(req.headers)
     const cookieSession = cookies.get("next-auth.session-token")
     const authenticated = cookieSession ? true : false
+    const session = await getServerSession(req, res, authOptions)
 
     // if you're going to dashboard and you are authenticated, proceed, else go to login page
     if (req.nextUrl.pathname.startsWith('/dashboard')) {
-        return authenticated ? NextResponse.rewrite(new URL('/dashboard', req.url)) : NextResponse.rewrite(new URL('/signings/login', req.url))
+        return session ? NextResponse.rewrite(new URL('/dashboard', req.url)) : NextResponse.rewrite(new URL('/signings/login', req.url))
     }
     
     // FIX the routing issue
