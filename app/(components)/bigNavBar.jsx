427a4a0d5bb6231372118cdecf499cde8b01a4f4
Emmanuel Taiwo <ttaiwo4910@gmail.com>
2023-09-28 18:09:03 +0100

    fixed navbar issue on laptop lg screens

--- app/(components)/bigNavBar.jsx
+++ app/(components)/bigNavBar.jsx
@@ -1,4 +1,5 @@
 import { useSession } from 'next-auth/react'
+import LogOut from './logOutBtn'
 import Image from 'next/image'
 import Link from 'next/link'
 import {useRouter} from 'next/navigation'
@@ -14,7 +15,7 @@ export default function bigNavBar({page}) {
         <div className="flex justify-between items-center">
             <p className='font-bold text-white px-2 py-1 bg-black rounded-md'>TradeUp</p>
             {/* <Image src="/images/3-19.jpg" width={10} height={10} alt="Logo"/>    */}
-            <div className='flex items-center gap-8 text-gray-5'>
+            <div className='flex items-center text-center gap-8 text-gray-5'>
                 {!authed?.user && <Link href="/signings/login" className=''>Log in</Link>}
                 {!authed?.user && <Link href="/signings/register" className=''>Sign up</Link>}
                 {(authed?.user && page == 'startPage') && <button onClick={() => router.push('/dashboard')}>Dashboard</button>}
