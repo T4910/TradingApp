dd90027d9d2b81ee3ef1ac879d62af78428ea9db
Emmanuel Taiwo <ttaiwo4910@gmail.com>
2023-09-29 17:50:51 +0100

    link to new email

--- app/(components)/footerContacts.jsx
+++ app/(components)/footerContacts.jsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 export default function footerContacts() {
   return (
     <div className='px-8 pt-8 '>
         <p className='mb-1'>Let's chat &#128064;&#9996;:</p>
-        <p className="text-start text-primary font-medium">+234 811 293 1362</p>
-        <p className="text-start text-primary font-medium">ttaiwo4910@gmail.com</p>
+        <p className="text-start text-primary font-medium">+000 000 000 0000</p>
+        <p className="text-start text-primary font-medium">
+          <a href="mailto://taiwo.the.techie@gmail.com">taiwo.the.techie@gmail.com</a>
+        </p>
     </div>
   )
 }
